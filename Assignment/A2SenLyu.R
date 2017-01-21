a9dae32de6c590c28fbc3c3945c2bc4605d7d3c0
sen.lyu <sen.lyu@uconn.edu>
2017-01-21 12:09:43 -0500

    finish homework

--- Assignment/A2SenLyu.R
+++ Assignment/A2SenLyu.R
@@ -10,5 +10,26 @@ median(stroke$age)
 # probs is numeric vector of probabilities with values in [0,1].
 quantile(stroke$age,probs = 0.75)
 #  Conduct a t-test to evaluate the hypothesis for the population age of 71 for age variable.
-# t.test could use to conduct a t-test
+# t.test(x,mu) could use to conduct a t-test, and the hypothesis that the mean is mu.
 t.test(stroke$age,mu=71)
+# Create a subset data frame called s1 of all patients who are not dead.  
+# use stroke$dead to call the dead colum. Use "," to get all the rest.
+# http://stackoverflow.com/questions/19205806/undefined-columns-selected-when-subsetting-data-frame
+s1=stroke[stroke$dead==F,]
+# Draw a plot of sex and age for these individuals.
+plot(stroke$sex, stroke$age)
+# What is your takeaway from the visual?
+# 1. male not dead has a bigger maximum and a smaller minimum.
+# 2. there are both outliers
+# 3. female has a bigger median, and a small IQR
+
+# What is the mean age of all patients who have diabetes and are not dead?
+# stroke$age[stroke$diab=="Yes" & stroke$dead==F] could call all the rows who is disable and not dead.
+# And because there are NA in the results, so if we need to calculate the mean, we need get rid of NA values
+# So we use na.rm = T
+mean(stroke$age[stroke$diab=="Yes" & stroke$dead==F],na.rm = T)
+
+# What is the mean age of all patients who have diabetes and are dead?
+mean(stroke$age[stroke$diab=="Yes" & stroke$dead==T],na.rm = T)
+
+
--- Assignment/assignment1.R
+++ Assignment/assignment1.R
@@ -2,6 +2,7 @@
 # Sen Lyu
 
 # Create a vector called x with a sequence of numbers from -10 to 10 incrementing by 0.1.
+# seq(a,b,by=c) means the sequence will start from a to b and the increasement will be c
 x=seq(-10,10,by = 0.1)
 # Create another vector called y which computes the sine (which is a trigonometric function; the syntax is sin()) of the values of x.
 y=sin(x)
@@ -12,8 +13,10 @@ title(main="Assignment Graph")
 # Write the expression in R to repeat x, y, and z as follows
 # a.Repeat 7 times as follows
 #"x" "y" "z" "x" "y" "z" "x" "y" "z" "x" "y" "z" "x" "y" "z" "x" "y" "z" "x" "y" "z"
+# rep(a,b) means repeat a, b times.
 rep(c("x","y","z"),7)
 # b.Repeat x, y 4 times and z twice as follows.
 #"x" "y" "x" "y" "x" "y" "x" "y" "z" "z"
+# So seperate the vector to "xyxyxyxy" and "zz", first rep(c("x","y"),c(4), to get xyxyxyxy, then rep("z",2) to get zz, at last conbine them.
 c(rep(c("x","y"),c(4)),rep("z",2))
 
